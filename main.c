fd4435da97a8b2704f44fb743c85728f3d44fe2b
Shams Parvez Arka <parvez6826@gmail.com>
2023-03-03 00:35:18 +0600

    pause functionality implimented

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#define SCREEN_WIDTH 1200
-#define SCREEN_HEIGHT 900
+#define SCREEN_WIDTH  1200.00
+#define SCREEN_HEIGHT  900.00
 
 void control_paddle();
 bool collided(Vector2 paddle);
@@ -14,6 +14,7 @@ bool collided(Vector2 paddle);
 Vector2 paddle1 = {10, 10};
 Vector2 paddle2 = {SCREEN_WIDTH - 30, SCREEN_HEIGHT - 70};
 Vector2 paddle_size  = {20, 80};
+const int paddle_speed = 12;
 
 Vector2 ball_size = {20, 20};
 Vector2 ball = {(float) SCREEN_WIDTH / 2, (float) SCREEN_HEIGHT / 2};
@@ -21,7 +22,11 @@ Vector2 ball_speed = {10, 10};
 
 int player1_score = 0;
 int player2_score = 0;
-const int font_size = 100;
+const int font_size = 40;
+const int font_size_title = 60;
+
+int pause = 0;
+int frame_counter = 0;
 
 int main()
 {
@@ -29,47 +34,59 @@ int main()
     SetTargetFPS(60);
     
     while(!WindowShouldClose()) {
-		DrawText(TextFormat("%d", player1_score), SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 100, font_size, WHITE);
-		DrawText(TextFormat("%d", player2_score), SCREEN_WIDTH / 2 +  25, SCREEN_HEIGHT / 2 - 100, font_size, WHITE);
-		
-        control_paddle();
-        
-		if (collided(paddle1)) {
-			ball_speed.x = - ball_speed.x;
-			ball.x += ball_speed.x;
-		}
-		else if (collided(paddle2)) {
-			ball_speed.x = - ball_speed.x;
-			ball.x += ball_speed.x;
-		}
-		else {
+		if (IsKeyPressed(KEY_SPACE)) 
+			pause = !pause;
+
+		if (!pause) {
+			control_paddle();
+			
+			if (collided(paddle1)) {
+				ball_speed.x = - ball_speed.x;
+				ball.x += ball_speed.x;
+			}
+			if (collided(paddle2)) {
+				ball_speed.x = - ball_speed.x;
+				ball.x += ball_speed.x;
+			}
+
 			if (ball.x + ball_size.x >= SCREEN_WIDTH) {
 				player1_score += 1;
 				ball.x = (float) SCREEN_WIDTH / 2;
 				ball.y = (float) SCREEN_HEIGHT / 2;
-			} else if (ball.x <= 0) {
+			} 
+			else if (ball.x <= 0) {
 				player2_score += 1;
 				ball.x = (float) SCREEN_WIDTH / 2;
 				ball.y = (float) SCREEN_HEIGHT / 2;
 			}
-			if (ball.y + ball_size.y >= SCREEN_HEIGHT) {
+			else if (ball.y + ball_size.y >= SCREEN_HEIGHT) {
 				ball_speed.y = - ball_speed.y;
 				ball.y = SCREEN_HEIGHT - ball_size.y;
-			} else if (ball.y <= 0) {
+			} 
+			else if (ball.y <= 0) {
 				ball_speed.y = - ball_speed.y;
 				ball.y = 0;
 			}
 			ball.x += ball_speed.x;
 			ball.y += ball_speed.y;
+			frame_counter = 0;
 		}
+		else 
+			frame_counter++;
 
-        BeginDrawing();
+		BeginDrawing();
 			ClearBackground(BLACK);
+			DrawText("PONG", SCREEN_WIDTH / 2 - 85, SCREEN_HEIGHT / 2 - 100, font_size_title, WHITE);
+			DrawText(TextFormat("%d", player1_score), SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 40, font_size, WHITE);
+			DrawText(TextFormat("%d", player2_score), SCREEN_WIDTH / 2 +  75, SCREEN_HEIGHT / 2 - 40, font_size, WHITE);
 			DrawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, GRAY);
 			DrawRectangleV(paddle1, paddle_size, WHITE);
 			DrawRectangleV(paddle2, paddle_size, WHITE);
-            DrawRectangleV(ball, ball_size, WHITE);
-        EndDrawing();
+			DrawRectangleV(ball, ball_size, WHITE);
+			if (pause && (frame_counter / 30) % 2) {
+				DrawText("paused", SCREEN_WIDTH / 2 - 171, SCREEN_HEIGHT / 2, 100, WHITE);
+			}
+		EndDrawing();
     }
 
     return 0;
@@ -77,24 +94,21 @@ int main()
 
 void control_paddle() {
     if (IsKeyDown(KEY_S) && (paddle1.y + paddle_size.y) <= SCREEN_HEIGHT) 
-        paddle1.y += 15;
+        paddle1.y += paddle_speed;
     if (IsKeyDown(KEY_W) && paddle1.y >= 0) 
-        paddle1.y -= 15;
+        paddle1.y -= paddle_speed;
     if (IsKeyDown(KEY_DOWN) && (paddle2.y + paddle_size.y) <= SCREEN_HEIGHT)
-        paddle2.y += 15;
+        paddle2.y += paddle_speed;
     if (IsKeyDown(KEY_UP) && paddle2.y >= 0) 
-        paddle2.y -= 15;    
+        paddle2.y -= paddle_speed;    
 }
 
 bool collided(Vector2 paddle) {
-	if (
-			paddle.x <= (ball.x + ball_size.x)   && 
-			(paddle.x + paddle_size.x) >= ball.x &&
-			paddle.y <= (ball.y + ball_size.y)   &&
-			(paddle.y + paddle_size.y) >= ball.y 
-	   ) 
-	{
+	if (paddle.x < (ball.x + ball_size.x)   && 
+		(paddle.x + paddle_size.x) > ball.x &&
+		paddle.y < (ball.y + ball_size.y)   &&
+		(paddle.y + paddle_size.y) > ball.y ) 
 		return true;
-	}
+
 	return false;
 }
